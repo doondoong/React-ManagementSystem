34347a9843a21708cb4bf9460ef965df0cc932dd
todaykim <pztdrd@naver.com>
2021-07-11 14:29:22 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -22,68 +22,60 @@ const styles = theme => ({
   }
 })
 
-const customers = [
-    {
-        id: 1,
-        image: "https://placeimg.com/64/64/1",
-        name: "홍길동",
-        birthday: "900101",
-        gender: "남자",
-        job: "대학생",
-    },
-    {
-        id: 2,
-        image: "https://placeimg.com/64/64/2",
-        name: "김자반",
-        birthday: "850206",
-        gender: "남자",
-        job: "프로그래머",
-    },
-    {
-        id: 3,
-        image: "https://placeimg.com/64/64/3",
-        name: "김둔둥",
-        birthday: "201123",
-        gender: "남자",
-        job: "아기",
-    }
-];
+
 
 class App extends Component {
-  render() {
-  const { classes } = this.props;
-  return (
-      <Paper className={classes.root}>
-          <Table className={classes.table}>
-              <TableHead>
-                  <TableRow>
-                      <TableCell>번호</TableCell>
-                      <TableCell>이미지</TableCell>
-                      <TableCell>이름</TableCell>
-                      <TableCell>생년월일</TableCell>
-                      <TableCell>성별</TableCell>
-                      <TableCell>직업</TableCell>
-                  </TableRow> 
-              </TableHead>
-              <TableBody>
-                  {customers.map((c) => {
-                      return (
-                          <Customer
-                              id={c.id}
-                              image={c.image}
-                              name={c.name}
-                              birthday={c.birthday}
-                              gender={c.gender}
-                              job={c.job}
-                              key={c.id}
-                          />
-                      );
-                  })}
-              </TableBody>
-          </Table>
-      </Paper>
-  );
-  }
+
+    state = {
+        customers: ""
+    };
+
+    componentDidMount() {
+        this.callApi()
+            .then(res => this.setState({customers: res}))
+            .catch(err => console.log(err));
+    }
+
+    callApi = async() => {
+        const response = await fetch('/api/customers');
+        const body = await response.json();
+        return body;
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <Paper className={classes.root}>
+                <Table className={classes.table}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>번호</TableCell>
+                            <TableCell>이미지</TableCell>
+                            <TableCell>이름</TableCell>
+                            <TableCell>생년월일</TableCell>
+                            <TableCell>성별</TableCell>
+                            <TableCell>직업</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {this.state.customers ? this.state.customers.map((c) => {
+                            return (
+                                <Customer
+                                    id={c.id}
+                                    image={c.image}
+                                    name={c.name}
+                                    birthday={c.birthday}
+                                    gender={c.gender}
+                                    job={c.job}
+                                    key={c.id}
+                                />
+                            );
+                        }) : ""}
+                    </TableBody>
+                </Table>
+            </Paper>
+        );
+    }
 }
 
 
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            id: 1,
+            image: "https://placeimg.com/64/64/1",
+            name: "홍길동",
+            birthday: "900101",
+            gender: "남자",
+            job: "대학생",
+        },
+        {
+            id: 2,
+            image: "https://placeimg.com/64/64/2",
+            name: "김자반",
+            birthday: "850206",
+            gender: "남자",
+            job: "프로그래머",
+        },
+        {
+            id: 3,
+            image: "https://placeimg.com/64/64/3",
+            name: "김둔둥",
+            birthday: "201123",
+            gender: "남자",
+            job: "아기",
+        },
+    ]);
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
